83fc11cf084f5337f168f95410cf16fdd649a616
kk <post@koljakutschera.de>
2018-10-30 23:55:15 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var cache = require('express-redis-cache')({ expire: 60 });
+
+cache.on('message', function (message) {
+  console.log("cache messege: ",message)
+});
+
+cache.on('connected', function () {
+  console.log("cache connected")
+});
+
+cache.on('disconnected', function () {
+  console.log("cache disconnected")
+});
+
+app.get('/', cache.route(), function (req, res) {
+  res.send('Hello World! We are shipping Nodejs-Version: ' + process.version);
+});
+
+app.listen(process.env.PORT || 8080, function () {
+  console.log('Example app listening on port:' + (process.env.PORT || 8080));
+});
